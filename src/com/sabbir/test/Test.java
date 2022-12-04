80cda915eba4766ca3c07a4e5ac5bf3186ef831e
rafitsabbir <rafitsabbir@gmail.com>
2022-12-04 17:21:58 +0600

    issue 2

--- src/com/sabbir/test/Test.java
+++ src/com/sabbir/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("test class for master issue1");
+		System.out.println("test class for master issue2");
 	}
 
 }
